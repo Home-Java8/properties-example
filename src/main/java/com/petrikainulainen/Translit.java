0fbb539b205ec7ba5da21990c25fcb13d1ae22f0
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-05-31 17:58:41 +0300

    #master

---
+++ src/main/java/com/petrikainulainen/Translit.java
@@ -0,0 +1,28 @@
+package com.petrikainulainen;
+
+/**
+ * @see https://stackoverflow.com/questions/16273318/transliteration-from-cyrillic-to-latin-icu4j-java
+ *      http://www.cyberforum.ru/java-j2se/thread2001566.html
+ */
+public class Translit {
+
+    public static void main(String[] args) {
+        String test = "Яблоко, ЯБЛОКО, ХОРОШО, ХоРоШо";
+        System.out.println("test ............... " + test);
+        System.out.println("toTranslit(test) ... " + transliterate(test));
+    }
+
+    public static String transliterate(String message){
+        char[] abcCyr =   {' ','а','б','в','г','д','е','ё', 'ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х', 'ц','ч', 'ш','щ','ъ','ы','ь','э', 'ю','я','А','Б','В','Г','Д','Е','Ё', 'Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х', 'Ц', 'Ч','Ш', 'Щ','Ъ','Ы','Ь','Э','Ю','Я','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        String[] abcLat = {" ","a","b","v","g","d","e","e","zh","z","i","y","k","l","m","n","o","p","r","s","t","u","f","h","ts","ch","sh","sch", "","i", "","e","ju","ja","A","B","V","G","D","E","E","Zh","Z","I","Y","K","L","M","N","O","P","R","S","T","U","F","H","Ts","Ch","Sh","Sch", "","I", "","E","Ju","Ja","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            for (int x = 0; x < abcCyr.length; x++ ) {
+                if (message.charAt(i) == abcCyr[x]) {
+                    builder.append(abcLat[x]);
+                }
+            }
+        }
+        return builder.toString();
+    }
+}
